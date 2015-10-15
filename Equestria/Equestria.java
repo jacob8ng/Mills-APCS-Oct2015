161941f1636878f1b280d6563b2ed4a07efb1031
jacob8ng <jacob8ng@yahoo.com>
2015-10-15 08:46:04 -0700

    Equestria

--- Equestria/Equestria.java
+++ Equestria/Equestria.java
@@ -16,11 +16,11 @@ public class Equestria{
     }
     public static void distance(){
         double a = twoCities(31, -16, 34, -9);
-        System.out.println(a+" miles");
+        System.out.println("Distance from Baltimare to Manehattan: "+a+" miles");
         double b = twoCities(7, -19, 22, -8);
-        System.out.println(b+" miles");
+        System.out.println("Distance from Los Pegasus to Neighagra Falls: "+b+" miles");
         double c = twoCities(25, -23, 16, -14);
-        System.out.println(c+" miles");
+        System.out.println("Distance from Badlands to Ponyville: "+c+" miles");
     }
     public static double twoCities(int x1, int y1, int x2, int y2){
         double distance = (int)((100)*(Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2))));
@@ -28,11 +28,12 @@ public class Equestria{
         return distance;
     }
     public static void circumference(){
-        double a = tour(25);
-        System.out.println(a+" miles");
+        double a = tour(7, -19, 34, -9);
+        System.out.println("Length of trip: "+a+" miles");
     }
-    public static double tour(int d){
-        double circumference = (int)((100)*(d*Math.PI));
+    public static double tour(int x1, int y1, int x2, int y2){
+        double distance = Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2));
+        double circumference = (int)((100)*(distance*Math.PI));
         circumference = circumference/100;
         return circumference;
     }
@@ -49,12 +50,12 @@ public class Equestria{
         double x = cosine(a, b, c);
         double y = cosine(b, c, a);
         double z = cosine(c, a, b);
-        System.out.println(a+" miles");
-        System.out.println(b+" miles");
-        System.out.println(c+" miles");
-        System.out.println(x+" degrees");
-        System.out.println(y+" degrees");
-        System.out.println(z+" degrees");
+        System.out.println("Distance between Baltimare and Los Pegasus: "+a+" miles");
+        System.out.println("Distance between Los Pegasus and Badlands: "+b+" miles");
+        System.out.println("Distance between Badlands and Baltimare: "+c+" miles");
+        System.out.println("Angle at Badlands: "+x+" degrees");
+        System.out.println("Angle at Baltimare: "+y+" degrees");
+        System.out.println("Angle at Los Pegasus: "+z+" degrees");
     }
     public static double pair(int x1, int y1, int x2, int y2){
         double distance = (int)((100)*(Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2))));
@@ -77,7 +78,7 @@ public class Equestria{
         double b = compare(city2x, city2y, city3x, city3y);
         double c = compare(city1x, city1y, city3x, city3y);
         double x = longest(a, b, c);
-        System.out.println(x+" miles");
+        System.out.println("Longest distance: "+x+" miles");
     }
     public static double compare(int x1, int y1, int x2, int y2){
         double distance = (int)((100)*(Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2))));
